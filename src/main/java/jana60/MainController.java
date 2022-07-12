c3d7bae6fd75bc317130098019600f4f72d83391
dacunzogaetano <dacunzogaetano@gmail.com>
2022-07-12 16:03:41 +0200

    bonus

--- src/main/java/jana60/MainController.java
+++ src/main/java/jana60/MainController.java
@@ -1,8 +1,13 @@
 package jana60;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/")
@@ -13,6 +18,14 @@ public class MainController {
 	    return "home";
 	  }
 	
+	
+	@GetMapping("/ora")
+	public String orario(Model model) {
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+		model.addAttribute("orario", now.format(formatter));
+		return "ora";
+	}
 	
 
 }
